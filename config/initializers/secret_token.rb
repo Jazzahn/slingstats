c9e0489c5baa8f04308adb1c600453a1b5a62658
Jonathan <jwhalley@slingbot.(none)>
2013-08-15 19:30:29 +0000

    Created Rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Slingstats::Application.config.secret_key_base = '2fe2ca9de5758a2836c654f2e2fe1bf78a6c5e271212e32442f458f9967f3be0bbcb7b2c9d0e87d1d68ebaaaaf92eb8f235e21c653cd6a74d227a4437b3d5187'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Slingstats::Application.config.session_store :cookie_store, key: '_slingstats_session'
