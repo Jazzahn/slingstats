88489b6c3f204dde931dc087692adeb56ad0742d
Marshel Helsper <marsol0x@gmail.com>
2014-01-16 21:56:52 -0500

    Adding Rails scopes to handle matching stats for players
    + 1v1
    + 2v2
    + 3v3

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,8 @@
 class Round < ActiveRecord::Base
   has_many :round_users
   has_many :users, :through => :round_users
+
+  scope :onevone, -> { where(:player_count => 2) }
+  scope :twovtwo, -> { where(:win_team_players => 2, :lose_team_players => 2) }
+  scope :threevthree, -> { where(:win_team_players => 3, :lose_team_players => 3) }
 end
