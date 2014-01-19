8e256081174fddd9a33174a398303d792e59014e
Marshel Helsper <marsol0x@gmail.com>
2014-01-18 20:58:15 -0500

    Configured a redirect when the results from a seearch is one
    This will have the browser redirect to a user's profile/stats page when
    a search result only returns with that user (results.count == 1)

--- app/controllers/user_search_controller.rb
+++ app/controllers/user_search_controller.rb
@@ -3,5 +3,7 @@ class UserSearchController < ApplicationController
 
   def search
     @results = User.search(params[:query])
+    redirect_to :controller => :player, :action => "show", :name => @results[0].username if @results.count == 1
+    @results
   end
 end
