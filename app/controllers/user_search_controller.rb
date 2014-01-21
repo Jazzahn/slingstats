100fe195a51a363d27310dce14453d4c6b757c61
Tim Krajcar <allegro@conmolto.org>
2014-01-21 15:01:21 -0700

    one more path fixer

--- app/controllers/user_search_controller.rb
+++ app/controllers/user_search_controller.rb
@@ -3,7 +3,7 @@ class UserSearchController < ApplicationController
 
   def search
     @results = User.search(params[:query])
-    redirect_to :controller => :player, :action => "show", :name => @results[0].username if @results.count == 1
+    redirect_to user_path(@results[0].username) if @results.count == 1
     @results
   end
 end
