67f811cc51f837525639f7e11152ca301b2ef815
Tim Krajcar <allegro@conmolto.org>
2014-01-21 21:56:21 -0700

    minor typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def not_found(txt = 'Not Found')
-  raise ActionController::RoutingError.new(txt)
-end
+    raise ActionController::RoutingError.new(txt)
+  end
 end
